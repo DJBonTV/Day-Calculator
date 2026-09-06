c055ffca3555876fe637706f9ca523e1360c37f2
DJBonTV <travers-cells03@icloud.com>
2026-09-06 22:23:19 +0100

    First Commit

---
+++ datecal.js
@@ -0,0 +1,8 @@
+function DateCal() {
+    const dayel = document.getElementById("day");
+    const monthel = document.getElementById("month");
+    const yearel = document.getElementById("year");
+    if (yearel % 4 === 0 && yearel % 100 !== 0 || yearel % 400 === 0) {
+        
+    }
+}
